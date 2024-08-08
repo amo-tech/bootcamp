040ef38a87d998a26b6caa827cf187d4eddd5884
amotech <56971237+amo-tech@users.noreply.github.com>
2024-08-08 21:09:48 +0900

    二分探索のRSpec追加

---
+++ spec/practice1/1-2_binary_search_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require_relative '../../code/practice1/1-2_binary_search.rb'
+
+RSpec.describe '#binary_search' do
+  it '配列に任意の要素があるときは配列のindexを返す' do
+    N = 8
+    A = [3, 5, 8, 10, 14, 17, 21, 39]
+    expect(binary_search(N, A)).to eq(2)
+  end
+
+  it '配列に任意の要素がないときは-1を返す' do
+    N = 9
+    A = [3, 5, 8, 10, 14, 17, 21, 39]
+    expect(binary_search(N, A)).to eq(-1)
+  end
+end
